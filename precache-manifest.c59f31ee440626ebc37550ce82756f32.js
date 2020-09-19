c2fef6886859457f54707f56c57fe6a9eca65559
princejr <ell17001@byui.edu>
2020-09-19 16:00:56 -0600

    Updates

---
+++ precache-manifest.c59f31ee440626ebc37550ce82756f32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa42c8faab7bb986534f7384b3b92a0e",
+    "url": "/web-frontend/index.html"
+  },
+  {
+    "revision": "96c011871bc3f6a356a6",
+    "url": "/web-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/web-frontend/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-frontend/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c011871bc3f6a356a6",
+    "url": "/web-frontend/static/js/main.63a7ecaf.chunk.js"
+  },
+  {
+    "revision": "441c3d049c311899e456",
+    "url": "/web-frontend/static/js/runtime-main.1058070e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
